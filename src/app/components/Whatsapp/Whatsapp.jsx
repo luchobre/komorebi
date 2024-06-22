7bde3a40ec6fd8d59e0bb4c85ae333cf9861a6f5
LuchoBre <lucho.bregoli@gmail.com>
2024-06-21 21:42:34 -0300

    se modifico cel

--- src/app/components/Whatsapp/Whatsapp.jsx
+++ src/app/components/Whatsapp/Whatsapp.jsx
@@ -8,7 +8,7 @@ const Whatsapp = () => {
   return (
 
           <Tooltip color={'success'} content={'Escribinos!'} className="capitalize">
-            <Link variant="flat" color={'success'} className="whatsappButton" href="https://wa.me/5491130141734?text=Hola!+me+interesa+saber+mas+sobre+Komorebi+Gin"
+            <Link variant="flat" color={'success'} className="whatsappButton" href="https://wa.me/5491144365097?text=Hola!+me+interesa+saber+mas+sobre+Komorebi+Gin"
       target="_blank">
               {
               <WhatsAppIcon style={{ color: 'green', height: '50px', width: '50px' }}/>
